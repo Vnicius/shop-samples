f6ac1924ab73cd95f5d529356a8484fc44f1d3dc
Unknown <leodantas96@gmail.com>
2018-09-19 21:02:37 -0300

    Doing Query

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
+const db = require('./database');
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("DB Connected!");
+});
+
+// db.query("INSERT INTO tecdb.categories (name) VALUES ('Livros');", function(err, rows, fields) {
+//   if (err) throw err;
+// });
+
+// db.query("SELECT * FROM tecdb.categories;", (err, rows, fields) => {
+//   if (err) throw err;
+//   rows.forEach(element => {
+//     console.log(element)
+//   });
+// });
 
 app.use("/", express.static("public"));
 
