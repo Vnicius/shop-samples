1809941a1dcf119ddf738a540a306b15ec27eb1f
LeoDantas <leodantas96@gmail.com>
2018-10-15 23:36:35 -0300

    Sessions (Doing)

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# TODO
+# Shop Samples
+
+## Iniciar Banco de Dados
+
+Utilizar o script **scriptbd.sql** para a criação do Banco de Dados SQL.
+
+## Iniciar Servidor
+
+1. Instale as dependências
+```
+    $ npm install
+```
+
+2. Inicializar o servidor
+```
+    $ npm start
+```
+
+3. Abrir **http://localhost:3000**
+
+## Fase 1
+
+![Imagem com descrição da fase 1](./images/Tarefa2.png)
+
+### Criação Da Interface do Usuário
+
+![Print da Página Inicial]()
+
+![Print da Página Do Produto]()
+
+## Fase 2
+
+Utilização do NodeJS e Express para roteamento das páginas.
+
+## Fase 3
+
+![Imagem com descrição da fase 3]()
+
+### CRUD dos Produtos
+
+![Print da tela de CRUD dos produtos]()
+
+### CRUD de Usuários
+
+![Print da tela de CRUD dos Usuários]()
+
+### Gerenciamento de Sessão
--- controllers/user.js
+++ controllers/user.js
@@ -1,59 +1,94 @@
-const TABLE = 'tecdb.users';
+const TABLE = "tecdb.users";
+const bcrypt = require("bcrypt");
+const cookies = require('cookies')
 
 const user = (server, dbConnection) => {
+  // Create
+  server.post("/api/v1/users", (req, res, next) => {
+    const { login, password } = req.body;
 
-    // Create
-    server.post('/api/v1/users', (req, res, next) => {
-        const {
-            login, password
-        } = req.body;
-        dbConnection.query(`INSERT INTO ${TABLE} (login, password) VALUES ('${login}', '${password}');`, (err, rows, fields) => {
-            if (err) throw err;
-            res.statusMessage = "Sucesso";
-            res.status(201); // OK
-            res.json({ ok: true });
-            next();
-        });
-    });
+    console.log(req.body);
 
-    // Read By Login
-    server.get('/api/v1/users/:login', (req, res, next) => {
-        if (req.params.login) {
-          const { login } = req.params;
-          dbConnection.query(`SELECT * FROM ${TABLE} WHERE login = ${login};`, (err, rows, fields) => {
-            if (err) throw err;
-            res.status(200);
-            res.json(rows);
-            next();
-          });
-        } else {
-          res.statusMessage = 'Login não encontrado';
+    let pass = bcrypt.hashSync(password, 10);
+
+    dbConnection.query(
+      `INSERT INTO ${TABLE} (login, password) VALUES ("${login}", "${pass}");`,
+      (err, rows, fields) => {
+        if (err) {
+          res.statusMessage = "Dados errados";
           res.status(400); // BAD REQUEST
-          res.json({ ok: false, message: 'Login não encontrado' });
+          res.json({ ok: false, message: "Dados errados" });
+          next();
+        } else {
+          res.statusMessage = "Sucesso";
+          res.status(201); // OK
+          res.json({ ok: true });
           next();
         }
-    });
+      }
+    );
+  });
 
+  // Read By Login
+  server.get("/api/v1/users/login", (req, res, next) => {
+    if (req.query.login) {
+      const { login, password } = req.query;
+      dbConnection.query(
+        `SELECT * FROM ${TABLE} WHERE login = "${login}";`,
+        
+        (err, rows, fields) => {
+           
+          if (err) throw err;
+          if (rows.length !== 0) {
 
-    // Delete
-    server.delete('/api/v1/users/:uid', (req, res, next) => {
-        if (req.params.uid) {
-            const { uid } = req.params;
-            dbConnection.query(`DELETE FROM ${TABLE} WHERE uid = ${uid}`, (err, rows, fields) => {
-            if (err) throw err;
-            res.statusMessage = "Sucesso";
-            res.status(204); // No Content
-            res.send();
-            next();
-            });
-        } else {
-            res.statusMessage = 'Ausência do ID do usuário';
-            res.status(400); // BAD REQUEST
-            res.json({ ok: false, message: 'Ausência do ID do usuário' });
-            next();
+            let data = rows[0];
+            bcrypt.compareSync(password, data.password, (err, equals) => {
+                
+                if (equals) {
+                  res.status(200);
+                  res.json({login: data.login, uid: data.uid});
+                  next();
+                } else {
+                  res.statusMessage = "Usuário ou Senha Incorretos";
+                  res.status(400); // BAD REQUEST
+                  res.json({ ok: false, message: "Usuario ou Senha Incorretos" });
+                  next();
+                }
+              });
+          }
+
         }
-    });
+    
+      );
+    } else {
+      res.statusMessage = "Usuario ou Senha Incorretos";
+      res.status(400); // BAD REQUEST
+      res.json({ ok: false, message: "Usuario ou Senha Incorretos" });
+      next();
+    }
+  });
 
-}
+  // Delete
+  server.delete("/api/v1/users/:uid", (req, res, next) => {
+    if (req.params.uid) {
+      const { uid } = req.params;
+      dbConnection.query(
+        `DELETE FROM ${TABLE} WHERE uid = ${uid}`,
+        (err, rows, fields) => {
+          if (err) throw err;
+          res.statusMessage = "Sucesso";
+          res.status(204); // No Content
+          res.send();
+          next();
+        }
+      );
+    } else {
+      res.statusMessage = "Ausência do ID do usuário";
+      res.status(400); // BAD REQUEST
+      res.json({ ok: false, message: "Ausência do ID do usuário" });
+      next();
+    }
+  });
+};
 
 module.exports = user;
